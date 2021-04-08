996f3ca31c2a250e0a0d564b34a04b61b5418a36
Alexander Carvalho <alexandersilva193@gmail.com>
2021-04-07 22:30:36 -0300

    Projeto criado

---
+++ src/main/java/com/acs/springbootmc/SpringbootMcApplication.java
@@ -0,0 +1,13 @@
+package com.acs.springbootmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acs/springbootmc/SpringbootMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acs.springbootmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
