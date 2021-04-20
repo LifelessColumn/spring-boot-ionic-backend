3e351142ff08934dc6bc15ddb3d01b6148a0671f
Alexander Carvalho <alexandersilva193@gmail.com>
2021-04-19 22:47:58 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/acs/springbootmc/dto/ClienteDTO.java
+++ src/main/java/com/acs/springbootmc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.acs.springbootmc.domain.Cliente;
+import com.acs.springbootmc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
